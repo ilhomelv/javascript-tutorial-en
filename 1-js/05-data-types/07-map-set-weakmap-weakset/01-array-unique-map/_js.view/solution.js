d05d98ac043f3c8cefdadff38cfdb9c7abb7e7d5
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-04-11 23:51:38 +0500

    Update solution.js

--- 1-js/05-data-types/07-map-set-weakmap-weakset/01-array-unique-map/_js.view/solution.js
+++ 1-js/05-data-types/07-map-set-weakmap-weakset/01-array-unique-map/_js.view/solution.js
@@ -1,3 +1,12 @@
+
+
+//pass array to a Set and then get it back as an array
+//arrays can have duplicates, Set does not allow duplicates, all duplicates inside Set collection will be removed
+
 function unique(arr) {
-  return Array.from(new Set(arr));
+   return Array.from(new Set(arr));
 }
+
+or
+
+let unique = arr => [...new Set(arr)];
